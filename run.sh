8ed022b71790840da0e28ef0e3403de67b981a84
Elliott Baron <ebaron@redhat.com>
2019-06-04 18:25:15 -0400

    Convert to multi-module project, update build.sh to use s2i

--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+COMMON_JAVA_ARGS="-Dcom.sun.management.jmxremote.rmi.port=9091 \
+-Dcom.sun.management.jmxremote=true \
+-Dcom.sun.management.jmxremote.port=9091 \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.local.only=false"
+'-Djava.rmi.server.hostname=robotcontroller'
+
 function cleanup() {
     set +e
     # TODO: better container management
