3998b6af08fff3e2b3bbd24cc8c9f166e67feb9e
Elliott Baron <ebaron@redhat.com>
2019-06-21 19:43:36 -0400

    Deploy images without building, set memory limits

--- minishift/setup.sh
+++ minishift/setup.sh
@@ -4,6 +4,8 @@ set -x
 set -e
 
 ROBOTS_SOURCE_REPO="${ROBOTS_SOURCE_REPO:-https://github.com/rh-jmc-team/jmc-robots-demo}"
+DEPLOY_ONLY="${DEPLOY_ONLY:-false}"
+IMAGE_REGISTRY="${IMAGE_REGISTRY:-quay.io/rh-jmc-team}"
 
 COMMON_JAVA_ARGS="-Dcom.sun.management.jmxremote.rmi.port=9091 \
 -Dcom.sun.management.jmxremote=true \
@@ -28,12 +30,20 @@ create_robot_app() {
     java_options="${COMMON_JAVA_ARGS} -Djava.rmi.server.hostname=${app_name}"
 
     echo "Creating application for subproject ${project_name}..."
-    oc new-app --name="${app_name}" \
-        --build-env="GRADLE_ARGS=${gradle_args}" \
-        --build-env="ARTIFACT_DIR=${project_name}/build" \
-        --build-env="ARTIFACT_COPY_ARGS=${copy_args}" \
-        --env="JAVA_OPTIONS=${java_options}" \
-        "${ROBOTS_BUILDER_IMAGE}~${ROBOTS_SOURCE_REPO}"
+    if [ "${DEPLOY_ONLY}" = false ]; then
+        oc new-app --name="${app_name}" \
+            --build-env="GRADLE_ARGS=${gradle_args}" \
+            --build-env="ARTIFACT_DIR=${project_name}/build" \
+            --build-env="ARTIFACT_COPY_ARGS=${copy_args}" \
+            --env="JAVA_OPTIONS=${java_options}" \
+            "${ROBOTS_BUILDER_IMAGE}~${ROBOTS_SOURCE_REPO}"
+    else
+        oc new-app --name="${app_name}" \
+            --env="JAVA_OPTIONS=${java_options}" \
+            "${IMAGE_REGISTRY}/${app_name}"
+    fi
+    # Impose some memory limits
+    oc set resources "dc/${app_name}" --limits="memory=256Mi"
     # Patch the service to expose port 9091, which isn't exposed by the builder image
     oc patch "svc/${app_name}" -p '{"spec": {"ports": [{"name": "9091-tcp", "port": 9091, "protocol": "TCP", "targetPort": 9091}]}}'
 }
@@ -45,7 +55,9 @@ fi
 
 oc new-project robots
 
-oc import-image --confirm "${ROBOTS_BUILDER_IMAGE}"
+if [ "${DEPLOY_ONLY}" = false ]; then
+    oc import-image --confirm "${ROBOTS_BUILDER_IMAGE}"
+fi
 
 create_robot_app RobotMakerExpress2000 robotmaker
 
@@ -53,7 +65,9 @@ create_robot_app RobotShop robotshop 'http://robotmaker:8080'
 
 create_robot_app RobotController robotcontroller 'http://robotmaker:8080'
 
-oc new-app quay.io/rh-jmc-team/container-jfr-web --name=container-jfr-web
+oc new-app "${IMAGE_REGISTRY}/container-jfr-web" --name=container-jfr-web
+# Impose some memory limits
+oc set resources "dc/container-jfr-web" --limits="memory=256Mi"
 
 oc delete svc container-jfr-web
 
@@ -61,13 +75,13 @@ oc expose dc container-jfr-web --target-port=8080 --port=80
 
 oc expose svc container-jfr-web
 
-oc new-app quay.io/rh-jmc-team/container-jfr --name=container-jfr
+oc new-app "${IMAGE_REGISTRY}/container-jfr" --name=container-jfr
+# Impose some memory limits
+oc set resources dc/container-jfr --limits="memory=256Mi"
 
 oc set env dc/container-jfr CONTAINER_JFR_DOWNLOAD_PORT="8080"
 
-oc expose dc container-jfr --name=container-jfr-exporter --port=8080
-
-oc expose svc container-jfr-exporter
+oc expose svc container-jfr --name=container-jfr-exporter --port=8080
 
 oc expose svc container-jfr --port=9090
 
